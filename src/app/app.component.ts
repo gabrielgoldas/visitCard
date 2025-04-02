9b4fcd73f5d4cda6c7c2708223b0908c0072ee04
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-04-02 16:14:59 -0300

    create the form and card visit in the app.componet, create the css of form too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'visit-card';
+  notValid?: string
+  submitted: boolean = false
+
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    fullname: ['', Validators.required],
+    age: ['', [Validators.required, Validators.max(120)]],
+    phone: ['', [
+      Validators.required,
+      Validators.pattern(/^\(\d{2}\) \d{5}-\d{4}/) // Expected form (51) 98888-7777
+    ]],
+    email: ['', [Validators.required, Validators.email]],
+    address: this.formBuilder.group({
+      street: ['', Validators.required],
+      number: ['', Validators.min(1)],
+      neighborhood: ['', Validators.required], // bairro
+      city: ['', Validators.required],
+    })
+  })
+
+  onCreate() {
+    if (this.form.invalid) {
+      this.notValid = 'Formulário inválido. Por favor, verifique os campos.'
+      return
+    } 
+    this.submitted = true
+    this.notValid = undefined
+  }
 }
